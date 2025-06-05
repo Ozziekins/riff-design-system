07dae82d273bfa9e8e735ee3cdb3b73f1a327fa0
Ozziekins <o.okonicha@innopolis.university>
2025-06-05 17:48:18 +0200

    Try to fix storybook deploy to gh-pages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,34 +1,31 @@
+// .storybook/main.ts
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-    // "!../src/stories/**" // Ignore default boilerplate
+  stories: [
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    // '!../src/stories/**' // Ignore default boilerplate
   ],
-  "addons": [
+  addons: [
     '@storybook/addon-links',
     '@storybook/addon-a11y',
     '@storybook/addon-docs',
     '@storybook/addon-designs',
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
   },
   staticDirs: [],
-  viteFinal: async (config) => {
-    config.base = '/riff-design-system/';
-    return config;
-  },
   typescript: {
-    reactDocgen: false, // DISABLE react-docgen
+    reactDocgen: false,
     check: true,
     reactDocgenTypescriptOptions: {},
   },
-
   docs: {
-    defaultName: "Docs"
+    defaultName: 'Docs',
   },
 };
+
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -2,32 +2,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Detect build mode
-const isLib = process.env.BUILD_LIB === 'true';
-
 export default defineConfig({
   plugins: [react()],
-  build: isLib
-    ? {
-        lib: {
-          entry: 'src/index.ts',
-          name: 'Riff',
-          fileName: (format) => `riff.${format}.js`,
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'Riff',
+      fileName: (format) => `riff.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
-        rollupOptions: {
-          external: ['react', 'react-dom'],
-          output: {
-            globals: {
-              react: 'React',
-              'react-dom': 'ReactDOM',
-            },
-          },
-        },
-      }
-    : {
-        outDir: 'dist', // for app build (gh-pages, Netlify, etc)
       },
-  server: {
-    open: true, // open browser on npm run dev
+    },
   },
 });
