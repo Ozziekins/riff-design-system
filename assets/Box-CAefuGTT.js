1991670e0b344aa78ac653ac3303c502e51cb190
Ozziekins <Ozziekins@users.noreply.github.com>
2025-06-06 08:12:05 +0000

    deploy: 3ef5952a5603f11d37020a0dc2db8b879d0b0508

--- assets/Box-CAefuGTT.js
+++
@@ -1,9 +0,0 @@
-import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{c as m,n as c}from"./colors-Cf755qi4.js";import{s as e}from"./spacing-CXxPM5Gd.js";import{r as u}from"./radii-D7JxPC8t.js";const $={none:"none",sm:"0 1px 2px rgba(0,0,0,0.05)",md:"0 2px 4px rgba(0,0,0,0.08)",lg:"0 4px 8px rgba(0,0,0,0.12)"},i={size0:"0px",size1:"4px",size2:"8px",size3:"12px",size4:"16px",size5:"20px",size6:"24px",size7:"32px",size8:"40px",size9:"48px",size10:"64px",size11:"80px",size12:"96px",size13:"128px",size14:"160px",size15:"192px",size16:"256px",size17:"300px",size18:"320px",size19:"384px",size20:"448px",size21:"512px",size22:"1024px",size23:"2048",size24:"3072px",sizeFull:"100%",sizeAuto:"auto",sizeMin:"min-content",sizeMax:"max-content"},d=c("div")`
-  background-color: ${({backgroundColor:s="transparent"})=>m[s]??s};
-  border-radius: ${({borderRadius:s="none"})=>u[s]??s};
-  padding: ${({padding:s="0"})=>e[s]??s};
-  margin: ${({margin:s="0"})=>e[s]??s};
-  box-shadow: ${({boxShadow:s})=>s?$[s]??s:"none"};
-  width: ${({width:s})=>s?i[s]:"auto"};
-  height: ${({height:s})=>s?i[s]:"auto"};
-`,B=({as:s="div",backgroundColor:p="transparent",borderRadius:x="none",padding:z=0,margin:n=0,boxShadow:o,children:t,...a})=>r.jsx(d,{as:s,backgroundColor:p,borderRadius:x,padding:z,margin:n,boxShadow:o,...a,children:t});export{B,i as a,$ as s};
---
+++ assets/Text-C4Q8pgb_.js
@@ -0,0 +1,8 @@
+import{j as m}from"./jsx-runtime-D_zvdyIk.js";import{c as p,n as c}from"./colors-BeVJvpkt.js";import{t}from"./typography-CGyrGtcf.js";import{s as f}from"./spacing-CXxPM5Gd.js";const g=c("span")`
+  font-family: ${t.fonts.base};
+  font-size: ${({fontSize:o="body"})=>t.sizes[o]};
+  font-weight: ${({fontWeight:o="regular"})=>t.weights[o]};
+  line-height: ${({lineHeight:o="normal"})=>t.lineHeights[o]};
+  color: ${({color:o="color-neutral-black"})=>p[o]??o};
+  margin-bottom: ${({marginBottom:o="0"})=>f[o]??o};
+`,x=({as:o="span",fontSize:r="body",fontWeight:s="regular",lineHeight:e="normal",color:a="color-neutral-black",marginBottom:n=0,children:l,...i})=>m.jsx(g,{as:o,fontSize:r,fontWeight:s,lineHeight:e,color:a,marginBottom:n,...i,children:l});export{x as T};
