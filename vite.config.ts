1047e82c5dc1899d71de3f32bf840f2b1f227cb3
Ozziekins <o.okonicha@innopolis.university>
2025-06-06 20:56:24 +0200

    Try to deploy storybook

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/riff-design-system/',
   plugins: [react()],
-  build: {
-    lib: {
-      entry: 'src/index.ts',
-      name: 'Riff',
-      fileName: (format) => `riff.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
-  },
 });
