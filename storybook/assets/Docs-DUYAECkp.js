09368ab71cde0e438d3953aeda8dc44777923864
Ozziekins <o.okonicha@innopolis.university>
2025-08-11 08:36:52 +0200

    Updates

---
+++ storybook/assets/Docs-DUYAECkp.js
@@ -0,0 +1,7 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as t}from"./index-Dg0d8cbw.js";import{M as o}from"./index-DD9zCW0b.js";import"./iframe-jDtwaOzH.js";import"./index-DHf1w5hM.js";import"./index-CyklGz79.js";function s(i){const n={a:"a",h1:"h1",h2:"h2",hr:"hr",p:"p",strong:"strong",...t(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(o,{title:"Docs/Docs"}),`
+`,e.jsx(n.h1,{id:"-design-guidelines",children:"🎨 Design Guidelines"}),`
+`,e.jsxs(n.p,{children:["This page contains design guidelines from the ",e.jsx(n.strong,{children:"Fender Play's Riff Design System"})," — maintained in Notion."]}),`
+`,e.jsxs(n.p,{children:["👉 ",e.jsx(n.a,{href:"https://sulfuric-brochure-f82.notion.site/Riff-Design-System-1ffa2e13c8058068b74ece79f158b377",rel:"nofollow",children:"View full Foundations & Principles on Notion"})]}),`
+`,e.jsx(n.hr,{}),`
+`,e.jsx(n.h2,{id:"visual-example",children:"Visual Example"}),`
+`,e.jsx("iframe",{src:"https://sulfuric-brochure-f82.notion.site/Riff-Design-System-1ffa2e13c8058068b74ece79f158b377",width:"100%",height:"1000px",style:{border:"none"}})]})}function m(i={}){const{wrapper:n}={...t(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(s,{...i})}):s(i)}export{m as default};
